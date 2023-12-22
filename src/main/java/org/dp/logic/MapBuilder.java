c63f296cdb2b94c608f6e943dc6c3a7d0860d6ab
a-little-dust <1429692815@qq.com>
2023-12-22 15:16:18 +0800

    地图（刚做到能读取json）

--- src/main/java/org/dp/logic/MapBuilder.java
+++ src/main/java/org/dp/logic/MapBuilder.java
@@ -1,17 +1,83 @@
 package org.dp.logic;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.dp.components.MapComponent;
 import org.dp.components.tiles.PlaceTileComponent;
 import org.dp.components.tiles.StartTileComponent;
 import org.dp.components.tiles.TileFactory;
 import org.dp.utils.Vector2i;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 // 使用生成器模式
 public class MapBuilder {
 
     // 设想是通过json生成地图，具体请诸位实现，现在的地图是硬编码进去的
-    public MapBuilder(String mapConfigJson){
+    public MapBuilder(String mapConfigJson,Vector2i pos){
+        TileFactory tileFactory = new TileFactory();
+        GameMap ans = new GameMap();
+        ans.mapComponent = new MapComponent(pos);
+        try {
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(mapConfigJson), "gbk");
+            BufferedReader br = new BufferedReader(isr);
+            StringBuffer resposeBuffer = new StringBuffer("");
+            String lineTxt = null;
+            String s = "";
 
+            //将文件内容全部拼接到 字符串s
+            while ((lineTxt = br.readLine()) != null) {
+                s += lineTxt;
+            }
+            //将文件内容转换为JSON对象
+            JSONObject object = JSON.parseObject(s);
+
+            //找到JSON数组的声明
+            JSONArray objs = object.getJSONArray("Tile");
+
+            Tile prev=null;
+            //遍历obj对象数组，逐个输出。
+            for (int i = 0; i < objs.size(); i++) {
+                JSONObject object1 = objs.getJSONObject(i);
+                int objId =Integer.parseInt(object1.getString("id")) ;
+                String objType = object1.getString("type");
+                float objName =Float.parseFloat(object1.getString("bonus")) ;
+                int objX =Integer.parseInt(object1.getString("x")) ;
+                int objY =Integer.parseInt(object1.getString("y")) ;
+                if(objId==1){
+                    Tile tile1 = new Tile();
+                    StartTileComponent startTileComponent = (StartTileComponent) tileFactory.getTile(TileFactory.TileType.START, new Vector2i(200,200), 0);
+                    tile1.component = startTileComponent;
+                    ans.firstTile = tile1;
+                    prev=tile1;
+                }
+                else if(objId<objs.size()){
+                    PlaceTileComponent placeTileComponent1 = (PlaceTileComponent) tileFactory.getTile(TileFactory.TileType.PLACE, new Vector2i(100,200), 1);
+                    Tile tile2 = new Tile();
+                    tile2.component = placeTileComponent1;
+                    prev.next = tile2;
+                    tile2.prev = prev;
+                    prev=tile2;
+                }
+                else if(objId==objs.size()){
+                    PlaceTileComponent placeTileComponent1 = (PlaceTileComponent) tileFactory.getTile(TileFactory.TileType.PLACE, new Vector2i(100,200), 1);
+                    Tile tile2 = new Tile();
+                    tile2.component = placeTileComponent1;
+                    prev.next = tile2;
+                    tile2.prev = prev;
+                    tile2.next=ans.firstTile;
+                }
+                //请帮我输出这个obj的所有相关信息 例如：id type name x y
+                System.out.println("id:" + objId + " type:" + objType + " bonus:" + objName + " x:" + objX + " y:" + objY);
+
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public GameMap build(Vector2i pos){
