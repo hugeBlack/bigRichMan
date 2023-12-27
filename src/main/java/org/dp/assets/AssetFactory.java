6ab847906d712e02083a1400797b6525c61d8e23
Karry <125780538+Iuriak@users.noreply.github.com>
2023-12-27 19:30:00 +0800

    享元模式加载房屋图片
    运用享元模式，新增BuildingAssets.java

--- src/main/java/org/dp/assets/AssetFactory.java
+++ src/main/java/org/dp/assets/AssetFactory.java
@@ -4,37 +4,40 @@ import java.util.HashMap;
 
 // 享元模式，加载图片
 public class AssetFactory {
-    public static HashMap<String, IAsset> assetMap=new HashMap<>();
+    public static HashMap<String, IAsset> assetMap = new HashMap<>();
 
     // 工厂应该不需要实例化？
-    private AssetFactory(){}
+    private AssetFactory() {
+    }
 
-    public static IAsset getAsset(String assetName){
+    public static IAsset getAsset(String assetName) {
 
-        if(assetMap.containsKey(assetName))
+        if (assetMap.containsKey(assetName))
             return assetMap.get(assetName);
 
         IAsset product;
 
-        if(assetName.equals("player")) {
+        if (assetName.equals("player")) {
             product = new PlayerPicture();
-        }else if(assetName.equals("fontLib")){
+        } else if (assetName.equals("fontLib")) {
             product = new FontLib();
-        }else if(assetName.equals("mapAssets")) {
+        } else if (assetName.equals("mapAssets")) {
             product = new MapAssets();
-        }else if(assetName.equals("titleSceneAssets")){
-                product = new TitleSceneAssets();
-        } else if(assetName.equals("confirmBoxAssets")){
+        } else if (assetName.equals("buildingAssets")){
+            product = new BuildingAssets();
+        } else if (assetName.equals("titleSceneAssets")) {
+            product = new TitleSceneAssets();
+        } else if (assetName.equals("confirmBoxAssets")) {
             product = new ConfirmBoxAssets();
-        } else if(assetName.equals("diceAssets")){
+        } else if (assetName.equals("diceAssets")) {
             product = new DiceAssets();
-        } else if(assetName.equals("testAssets")){
+        } else if (assetName.equals("testAssets")) {
             product = new TestComponentAssets();
         } else if (assetName.equals("playerInfo")) {
-            product=new PlayerInfo();
+            product = new PlayerInfo();
         } else if (assetName.equals("imageAssets")) {
-            product=new ImageAsset();
-        }else{
+            product = new ImageAsset();
+        } else {
             throw new RuntimeException("No such product!");
         }
         assetMap.put(assetName, product);
