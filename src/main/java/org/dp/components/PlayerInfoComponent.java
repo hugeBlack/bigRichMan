6f05a68be7d6590b0aeb401b6463a38ebe80930c
Karry <125780538+Iuriak@users.noreply.github.com>
2023-12-27 19:19:46 +0800

    玩家信息优化
    新增点券、是否在医院等玩家信息，增加房屋购置部分代码

--- src/main/java/org/dp/components/PlayerInfoComponent.java
+++ src/main/java/org/dp/components/PlayerInfoComponent.java
@@ -1,4 +1,5 @@
 package org.dp.components;
+
 import org.dp.assets.*;
 import org.dp.event.ButtonClickEvent;
 import org.dp.logic.GameSystem;
@@ -19,22 +20,23 @@ import org.dp.view.events.MouseEvent;
 import java.awt.*;
 import java.util.LinkedList;
 
-public class PlayerInfoComponent extends Component{
-    private ImageAsset assets =( ImageAsset) AssetFactory.getAsset("imageAssets");
+public class PlayerInfoComponent extends Component {
+    private ImageAsset assets = (ImageAsset) AssetFactory.getAsset("imageAssets");
     PlayerPicture playerPicture = (PlayerPicture) AssetFactory.getAsset("player");
     private GameButton exitButton;
     private Vector2i windowPos;
-    private PlayerInfoComponent me;//自己
-    PlayerInfo playerInfos =GameSystem.get().getPlayerInfo();
+    private PlayerInfoComponent me;// 自己
+    PlayerInfo playerInfos = GameSystem.get().getPlayerInfo();
+
     public PlayerInfoComponent() {
-        super(new Vector2i(200,100), new Vector2i(800,600));
+        super(new Vector2i(200, 100), new Vector2i(800, 600));
         me = this;
-//添加按钮
-        exitButton = new GameButton(new Vector2i(500, 640), new Vector2i(128,35), "返回");
+        // 添加按钮
+        exitButton = new GameButton(new Vector2i(500, 640), new Vector2i(128, 35), "返回");
         exitButton.registerObserver(new ComponentObserver() {
             @Override
             public void onEvent(ComponentEvent e) {
-                if(e instanceof ButtonClickEvent){
+                if (e instanceof ButtonClickEvent) {
                     Playground.get().removeChildren(me);
                 }
             }
@@ -52,40 +54,41 @@ public class PlayerInfoComponent extends Component{
         graphics.setFont(font);
         graphics.setColor(Color.gray);
         setHitBoxSize(getParent().getHitBoxSize());
-        graphics.fillRect(windowPos.x,windowPos.y, board.getWidth(null), board.getHeight(null));
+        graphics.fillRect(windowPos.x, windowPos.y, board.getWidth(null), board.getHeight(null));
         graphics.drawImage(board, windowPos.x, windowPos.y, null);
         graphics.setFont(font);
         graphics.setColor(Color.BLACK);
-        //做一个for循环，i的范围是0到玩家数量-1，要输出这个玩家的所有信息
-        for(int i=0;i<GameSystem.get().getPlayerNum();i++){
-            //获取这个玩家
-            //注意要根据选择的角色来获取玩家
-            PlayerInfo player=playerInfos.getPlayerInfo(GameSystem.get().getActorChoose()[i]);
-            int dist=900/GameSystem.get().getPlayerNum();
-            int x=windowPos.x+200+dist*i;
-            graphics.drawImage(playerPicture.img[GameSystem.get(). getActorChoose()[i]], x, windowPos.y+50, 100, 100, null);
-            int y=windowPos.y+200;
-            //输出标题：玩家1
-            graphics.drawString("玩家"+(i+1),x,y);
-            y-=40;
-            //输出玩家的信息
-            graphics.drawString(player.defaultName,x,y+150);
-            graphics.drawString("金币数："+player.coinNum,x,y+230);
-            graphics.drawString("地产数："+player.houseNum,x,y+310);
-            //输出卡牌数
-            graphics.drawString("卡牌数："+player.cardNum,x,y+390);
+        // 做一个for循环，i的范围是0到玩家数量-1，要输出这个玩家的所有信息
+        for (int i = 0; i < GameSystem.get().getPlayerNum(); i++) {
+            // 获取这个玩家
+            // 注意要根据选择的角色来获取玩家
+            PlayerInfo player = playerInfos.getPlayerInfo(GameSystem.get().getActorChoose()[i]);
+            int dist = 900 / GameSystem.get().getPlayerNum();
+            int x = windowPos.x + 200 + dist * i;
+            graphics.drawImage(playerPicture.img[GameSystem.get().getActorChoose()[i]], x, windowPos.y + 50, 100, 100,
+                    null);
+            int y = windowPos.y + 200;
+            // 输出标题：玩家1
+            graphics.drawString("玩家" + (i + 1), x, y);
+            y -= 40;
+            // 输出玩家的信息
+            graphics.drawString(player.defaultName, x, y + 150);
+            graphics.drawString("金币数：" + player.coinNum, x, y + 230);
+            graphics.drawString("点券数：" + player.couponNum, x, y + 280);
+            graphics.drawString("地产数：" + player.houseNum, x, y + 330);
+            // 输出卡牌数
+            graphics.drawString("卡牌数：" + player.cardNum, x, y + 380);
         }
 
     }
 
     @Override
-    public boolean onMouseEventMe(MouseEvent e){
+    public boolean onMouseEventMe(MouseEvent e) {
         return true;
     }
 
     public void show() {
         Playground.get().addComponent(this);
     }
-
 
 }
--- src/main/java/org/dp/components/tiles/StartTileComponent.java
+++ src/main/java/org/dp/components/tiles/StartTileComponent.java
@@ -10,12 +10,13 @@ import java.awt.*;
 public class StartTileComponent extends TileComponent {
 
     private Image img = ((MapAssets) AssetFactory.getAsset("mapAssets")).startTile;
-private String title="起点";
-private int playerID;
-private String Price="+ 1000";
-    public StartTileComponent(Vector2i p,int playerID) {
+    private String title = "起点";
+    private int playerID;
+    private String Price = "+ 1000";
+
+    public StartTileComponent(Vector2i p, int playerID) {
         super(p);
-        this.playerID=playerID;
+        this.playerID = playerID;
     }
 
     @Override
@@ -23,11 +24,11 @@ private String Price="+ 1000";
         Vector2i p = getAbsPosition();
         Vector2i hitBox = getHitBoxSize();
         int ascent = graphics.getFontMetrics().getAscent();
-        graphics.drawImage(img, p.x,p.y, 100,100,null);
-        Font font = ((FontLib)AssetFactory.getAsset("fontLib")).placePriceFont;
+        graphics.drawImage(img, p.x, p.y, 100, 100, null);
+        Font font = ((FontLib) AssetFactory.getAsset("fontLib")).placePriceFont;
         graphics.setFont(font);
         graphics.setColor(Color.white);
-        graphics.drawString(Price, p.x + hitBox.x/4  , p.y + 70  + ascent);
-        graphics.drawString(title, p.x + hitBox.x/4 , p.y + 10  + ascent);
+        graphics.drawString(Price, p.x + hitBox.x / 4, p.y + 70 + ascent);
+        graphics.drawString(title, p.x + hitBox.x / 4, p.y + 10 + ascent);
     }
 }
